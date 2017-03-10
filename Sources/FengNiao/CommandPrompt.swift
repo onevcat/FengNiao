97d38604ad26136a992eccdeec349de56627e5e9
onevcat <onevcat@gmail.com>
2017-03-10 15:41:10 +0900

    Remove swift lane dependency

--- Sources/FengNiao/CommandPrompt.swift
+++ Sources/FengNiao/CommandPrompt.swift
@@ -6,7 +6,6 @@
 //
 //
 
-import Swiftline
 import Rainbow
 import FengNiaoKit
 
@@ -16,13 +15,18 @@ public enum Action {
     case ignore
 }
 
-
 public func promptResult(files: [FileInfo]) -> Action {
     let size = files.reduce(0) { $0 + $1.size }.fn_readableSize
     print("\(files.count) unused files are found. Total Size: \(size)".yellow.bold)
-    return choose("What do you want to do with them? ".bold, type: Action.self) { settings in
-        settings.addChoice("list") { .list }
-        settings.addChoice("delete") { .delete}
-        settings.addChoice("ignore") { .ignore}
+    print("What do you want to do with them? (l)ist|(d)elete|(i)gnore".bold, terminator: " ")
+    
+    guard let result = readLine() else {
+        return promptResult(files: files)
+    }
+    switch result {
+    case "l", "L": return .list
+    case "d", "D": return .delete
+    case "i", "I": return .ignore
+    default: return promptResult(files: files)
     }
 }
