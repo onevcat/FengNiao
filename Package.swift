47846908f43fff0ba05483ec5141a05a11115d97
Séraphin Huguenot <shuguenot@manager.one>
2021-09-27 12:03:37 +0200

    Upgrade PathKit + Spectre

--- Package.swift
+++ Package.swift
@@ -10,8 +10,8 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/onevcat/Rainbow.git", from: "3.1.1"),
         .package(url: "https://github.com/benoit-pereira-da-silva/CommandLine.git", from: "4.0.0"),
-        .package(url: "https://github.com/kylef/Spectre.git", from: "0.9.0"),
-        .package(url: "https://github.com/kylef/PathKit.git", from: "0.9.0")
+        .package(url: "https://github.com/kylef/Spectre.git", from: "0.10.1"),
+        .package(url: "https://github.com/kylef/PathKit.git", from: "1.0.1")
     ],
     targets: [
         .target(name: "FengNiaoKit", dependencies: ["Rainbow", "PathKit"]),
