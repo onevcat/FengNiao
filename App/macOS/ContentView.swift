25af7bb1f5a48302063da3f24d253b3f0b98d0f9
Khoa Le <ldakhoa1308@gmail.com>
2023-09-01 15:06:08 +0700

    Setup macOS project

---
+++ App/macOS/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  fengniao
+//
+//  Created by Khoa Le on 01/09/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ App/macOS/FengNiaoApp.swift
@@ -0,0 +1,17 @@
+//
+//  FengNiaoApp.swift
+//  fengniao
+//
+//  Created by Khoa Le on 01/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct FengNiaoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
